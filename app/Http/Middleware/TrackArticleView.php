1bc4c0496bcf2f091b58091a4769354bcdaf37a5
GabeSilvaDev <spanxscomercial@gmail.com>
2025-11-17 22:54:37 -0300

    ✨ feat: criar middleware para rastreamento automático de visualizações

---
+++ app/Http/Middleware/TrackArticleView.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Contracts\ArticleRankingServiceInterface;
+use App\Models\Article;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrackArticleView
+{
+    public function __construct(
+        private readonly ArticleRankingServiceInterface $rankingService
+    ) {}
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        if (! $request->isMethod('GET')) {
+            return $response;
+        }
+
+        /** @var Article|null $article */
+        $article = $request->route('article');
+
+        if ($article instanceof Article) {
+            $articleId = $article->id;
+            assert(is_string($articleId));
+
+            $this->rankingService->incrementView($articleId);
+
+            $article->withoutVersioning(function () use ($article): void {
+                $article->increment('view_count');
+            });
+        }
+
+        return $response;
+    }
+}
