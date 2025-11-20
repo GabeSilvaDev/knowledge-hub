44a2115f35e052d2d73e43bea1e0d6daf787cf5b
GabeSilvaDev <spanxscomercial@gmail.com>
2025-11-20 17:38:54 -0300

    ✨ feat: adicionar novas rotas para funcionalidades sociais
    - Adicionar rotas de comentários (/api/comments, /api/articles/{id}/comments)
    - Adicionar rotas de likes (/api/articles/{id}/like)
    - Adicionar rotas de seguidores (/api/users/{id}/follow, /followers, /following)
    - Adicionar rotas de feed (/api/feed, /api/feed/public)
    - Adicionar rotas de perfil (/api/me, /api/users/{id})
    - Implementar throttling específico: comments (30/min), likes (60/min), follow (30/min), articles (10/min)
    - Separar rotas públicas de autenticadas

--- routes/api.php
+++ routes/api.php
@@ -3,27 +3,51 @@
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ArticleRankingController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\FeedController;
+use App\Http\Controllers\FollowerController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\UserController;
 use App\Http\Middleware\TrackArticleView;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('/feed', [FeedController::class, 'index']);
+Route::get('/feed/public', [FeedController::class, 'public']);
+
 Route::get('/articles/popular', [ArticleController::class, 'popular']);
 Route::get('/articles/ranking', [ArticleRankingController::class, 'index']);
 Route::get('/articles/ranking/statistics', [ArticleRankingController::class, 'statistics']);
+Route::get('/articles/{article}', [ArticleController::class, 'show'])->middleware(TrackArticleView::class);
+
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::get('/users/{user}/followers', [FollowerController::class, 'followers']);
+Route::get('/users/{user}/following', [FollowerController::class, 'following']);
+
+Route::get('/articles/{articleId}/comments', [CommentController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function (): void {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/revoke-all', [AuthController::class, 'revokeAll']);
+    Route::get('/me', [UserController::class, 'me']);
+    Route::put('/me', [UserController::class, 'update']);
 
     Route::post('/articles/ranking/sync', [ArticleRankingController::class, 'sync']);
-
-    Route::post('/articles', [ArticleController::class, 'store']);
+    Route::post('/articles', [ArticleController::class, 'store'])->middleware('throttle:10,1');
     Route::put('/articles/{article}', [ArticleController::class, 'update']);
     Route::delete('/articles/{article}', [ArticleController::class, 'destroy']);
     Route::get('/articles', [ArticleController::class, 'index']);
     Route::get('/articles/{article}/ranking', [ArticleRankingController::class, 'show']);
-});
 
-Route::get('/articles/{article}', [ArticleController::class, 'show'])->middleware(TrackArticleView::class);
+    Route::post('/comments', [CommentController::class, 'store'])->middleware('throttle:30,1');
+    Route::put('/comments/{comment}', [CommentController::class, 'update']);
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
+
+    Route::post('/articles/{article}/like', [LikeController::class, 'toggle'])->middleware('throttle:60,1');
+    Route::get('/articles/{article}/like/check', [LikeController::class, 'check']);
+
+    Route::post('/users/{user}/follow', [FollowerController::class, 'toggle'])->middleware('throttle:30,1');
+    Route::get('/users/{user}/follow/check', [FollowerController::class, 'check']);
+});
